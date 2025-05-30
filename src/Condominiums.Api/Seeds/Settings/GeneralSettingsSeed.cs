0d5004977627b4da1ba45a8ea8ce3d19593ab3cb
Oscar David Díaz <lechediaz@gmail.com>
2025-05-29 22:54:00 -0500

    Se agrega información acerca de la licencia en cada archivo de código

--- src/Condominiums.Api/Seeds/Settings/GeneralSettingsSeed.cs
+++ src/Condominiums.Api/Seeds/Settings/GeneralSettingsSeed.cs
@@ -1,4 +1,20 @@
+/*
+API de Nami SIC, la aplicación de código abierto que permite administrar Condominios fácilmente.
+Copyright (C) 2025  Oscar David Díaz Fortaleché  lechediaz@gmail.com
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 using AutoMapper;
 using Condominiums.Api.Models.DTOs.Settings;
 using Condominiums.Api.Options;
@@ -33,7 +49,7 @@ public class GeneralSettingsSeed : IGeneralSettingsSeed
 
         if (generalSettingsDb == null)
         {
-            _logger.LogInformation("Initializing '{0}' settings", _generalSettingsStore.Name);
+            _logger.LogInformation("Initializing '{name}' settings", _generalSettingsStore.Name);
             GeneralSettingsDto generalSettings = _mapper.Map<GeneralSettingsDto>(_generalSettingsOptions);
             await _generalSettingsStore.InsertAsync(generalSettings);
         }
